9a9d594db5f34b84d112056e5607ab953f0790be
enlighten0707 <enlighten@sjtu.edu.cn>
2021-05-18 13:02:56 +0800

    Complete connection between frontend and backend

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'FaceRecog',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +49,37 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+ 'http://127.0.0.1:9527',
+ 'http://localhost:9527',
+)
+CORS_ALLOW_CREDENTIALS = True # 指明在跨域访问中，后端是否支持对cookie的操作。
+CORS_ALLOW_METHODS = (
+ 'DELETE',
+ 'GET',
+ 'OPTIONS',
+ 'PATCH',
+ 'POST',
+ 'PUT',
+ 'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+ 'XMLHttpRequest',
+ 'X_FILENAME',
+ 'accept-encoding',
+ 'authorization',
+ 'content-type',
+ 'dnt',
+ 'origin',
+ 'user-agent',
+ 'x-csrftoken',
+ 'x-requested-with',
+ 'Pragma',
+)
 
 ROOT_URLCONF = 'backend.urls'
 
--- frontend/src/views/vehicle/index.vue
+++ frontend/src/views/vehicle/index.vue
@@ -5,12 +5,15 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
   name: 'Vehicle',
   methods: {
     guide() {
       console.log(1)
+      axios.post('http://127.0.0.1:8000/FaceRecog/Hello/').then((res)=>(
+        console.log(res.data)
+      ))
     }
   }
 }
