562fbe081debb6adbd39ef80a4566646b6ecc9af
nian0616 <1501624750@qq.com>
2021-06-11 11:18:11 +0800

    web_and_db

--- frontend/src/views/documentation/index.vue
+++ frontend/src/views/documentation/index.vue
@@ -1,76 +1,75 @@
 <template>
   <div class="dashboard-editor-container">
-    <panel-group @handleSetLineChartData="handleSetLineChartData" />
-    
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData" />
-    </el-row>
+    <panel-group @handleSetBarChartData="handleSetBarChartData" />
 
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <line-chart :chart-data="barChartData" />
+    </el-row>
   </div>
 </template>
 
 <script>
-import GithubCorner from '@/components/GithubCorner'
 import PanelGroup from './components/PanelGroup'
 import LineChart from './components/LineChart'
-import RaddarChart from './components/RaddarChart'
-import PieChart from './components/PieChart'
-import BarChart from './components/BarChart'
-import TransactionTable from './components/TransactionTable'
-import TodoList from './components/TodoList'
-import BoxCard from './components/BoxCard'
-
-const lineChartData = {
-  price: {
-    expectedData: [590, 592, 600, 630, 610, 620, 600],
-    dim: '$',
-  },
-  change_num: {
-    expectedData: [9.9, 9.2, 9.5, 10.2, 9.7, 9.8, 10],
-    dim: '$',
-  },
-  change_ratio: {
-    expectedData: [0.8, 1.1, 1.2, 1.0, 0.9, 1.3, 1.5],
-    dim: '%',
-  },
-  shoppings: {
-    expectedData: [19.5, 20.5, 20.3, 18.9, 20.2, 19, 20],
-    dim:'万',
-  },
-  // humidity_soil: {
-  //   expectedData: [1230, 140, 141, 142, 145, 150, 160],
-  //   dim:'%',
-  // },
-  // rain: {
-  //   expectedData: [130, 140, 141, 142, 145, 150, 160],
-  //   dim:'%',
-  // }
-}
+import axios from 'axios'
 
 export default {
   name: 'DashboardAdmin',
   components: {
-    GithubCorner,
     PanelGroup,
     LineChart,
-    RaddarChart,
-    PieChart,
-    BarChart,
-    TransactionTable,
-    TodoList,
-    BoxCard
   },
   data() {
     return {
-      lineChartData: lineChartData.price
+      barChartData: {
+        name: [],
+        expectedData: [],
+        dim: '',
+        color: ''
+      }
     }
   },
+
   methods: {
-    handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
+    give_data(type, data) {
+          this.barChartData.name = data.name;
+          switch(type){
+          case 'price':
+            this.barChartData.expectedData = data.price;
+            this.barChartData.dim = '农作物价格';
+            this.barChartData.color = 'pink';
+            break;
+          case 'change_num':
+            this.barChartData.expectedData = data.change_num;
+            this.barChartData.dim = '涨跌数值';
+            this.barChartData.color = 'blue';
+            break;
+          case 'change_ratio':
+            this.barChartData.expectedData = data.change_ratio;
+            this.barChartData.dim = '涨跌幅度';
+            this.barChartData.color = 'green';
+            break;
+          case 'trading_volumes':
+            this.barChartData.expectedData = data.trading_volumes;
+            this.barChartData.dim = '成交量';
+            this.barChartData.color = 'red';
+            break;
+          }
+      },
+    handleSetBarChartData(type){
+      axios({
+         method: 'get',
+         url: 'http://127.0.0.1:8000/ProdInfo/get_data/',
+         responseType: 'json'
+       })
+      .then((response)=>(
+        console.log(response.data),
+        this.give_data(type, response.data)
+      ))
     }
-  }
+  },
 }
+
 </script>
 
 <style lang="scss" scoped>
