5fa18911d2f227bb1a0d77fd7c0b8d94f55339bc
enlighten0707 <enlighten@sjtu.edu.cn>
2021-06-18 09:55:02 +0800

    Modifiy

--- frontend/src/views/documentation/index.vue
+++ frontend/src/views/documentation/index.vue
@@ -20,33 +20,34 @@ import TransactionTable from './components/TransactionTable'
 import TodoList from './components/TodoList'
 import BoxCard from './components/BoxCard'
 
-const lineChartData = {
-  price: {
-    expectedData: [590, 592, 600, 630, 610, 620, 600],
-    dim: '$',
-  },
-  change_num: {
-    expectedData: [9.9, 9.2, 9.5, 10.2, 9.7, 9.8, 10],
-    dim: '$',
-  },
-  change_ratio: {
-    expectedData: [0.8, 1.1, 1.2, 1.0, 0.9, 1.3, 1.5],
-    dim: '%',
-  },
-  shoppings: {
-    expectedData: [19.5, 20.5, 20.3, 18.9, 20.2, 19, 20],
-    dim:'万',
-  },
-  // humidity_soil: {
-  //   expectedData: [1230, 140, 141, 142, 145, 150, 160],
-  //   dim:'%',
-  // },
-  // rain: {
-  //   expectedData: [130, 140, 141, 142, 145, 150, 160],
-  //   dim:'%',
-  // }
-}
+// const lineChartData = {
+//   price: {
+//     expectedData: [590, 592, 600, 630, 610, 620, 600],
+//     dim: '$',
+//   },
+//   change_num: {
+//     expectedData: [9.9, 9.2, 9.5, 10.2, 9.7, 9.8, 10],
+//     dim: '$',
+//   },
+//   change_ratio: {
+//     expectedData: [0.8, 1.1, 1.2, 1.0, 0.9, 1.3, 1.5],
+//     dim: '%',
+//   },
+//   shoppings: {
+//     expectedData: [19.5, 20.5, 20.3, 18.9, 20.2, 19, 20],
+//     dim:'万',
+//   },
+//   // humidity_soil: {
+//   //   expectedData: [1230, 140, 141, 142, 145, 150, 160],
+//   //   dim:'%',
+//   // },
+//   // rain: {
+//   //   expectedData: [130, 140, 141, 142, 145, 150, 160],
+//   //   dim:'%',
+//   // }
+// }
 
+import axios from 'axios'
 export default {
   name: 'DashboardAdmin',
   components: {
@@ -62,12 +63,19 @@ export default {
   },
   data() {
     return {
-      lineChartData: lineChartData.price
+      // lineChartData: lineChartData.price,
+      lineChartData: {
+        expectedData: [],
+        dim: '',
+      }
     }
   },
   methods: {
     handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
+      // this.lineChartData = lineChartData[type]
+      this.lineChartData.expectedData = [1,2,3];
+      this.lineChartData.dim='$';
+      console.log(this.lineChartData);
     }
   }
 }
